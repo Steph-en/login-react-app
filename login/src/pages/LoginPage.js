d0b0fe0c2986681609def13bb7d3bbdc0d2557c4
Steph-en <appahstephen9@gmail.com>
2023-12-11 14:02:00 +0000

    Feat: added icons to the input fields

--- login/src/pages/LoginPage.js
+++ login/src/pages/LoginPage.js
@@ -14,8 +14,24 @@ import GoogleIcon from '@mui/icons-material/Google';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 
 export default function SignIn({ doSmth }) {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
       <Paper elevation={6} sx={{ my: 13, borderRadius: ".5em .5em .5em .5em", }}>
@@ -78,26 +94,63 @@ export default function SignIn({ doSmth }) {
                 </Typography>
               </Grid>
               <Box component="form" noValidate sx={{ mt: 1 }}>
-                <TextField
+                {/* <TextField
                   margin="normal"
                   required
                   fullWidth
                   id="email"
-                  label="Email Address"
+                  label="Username"
                   name="email"
                   autoComplete="email"
                   autoFocus
-                />
-                <TextField
+                /> */}
+                <FormControl sx={{ mt: 1, }}
                   margin="normal"
                   required
                   fullWidth
-                  name="password"
+                  id="username"
                   label="Password"
-                  type="password"
+                  name="username"
+                  autoComplete="email"
+                  variant="outlined"
+                  autoFocus
+                >
+                  <InputLabel htmlFor="outlined-adornment-username">Username</InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-username"
+                    endAdornment={<InputAdornment position="end"><AccountCircleRoundedIcon /></InputAdornment>}
+                    label="Username"
+                  />
+                </FormControl>
+                <FormControl sx={{ mt: 1, }}
+                  margin="normal"
+                  required
+                  fullWidth
                   id="password"
-                  autoComplete="current-password"
-                />
+                  label="Password"
+                  name="password"
+                  autoComplete="password"
+                  variant="outlined"
+                >
+                  <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showPassword ? 'text' : 'password'}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Password"
+                  />
+                </FormControl>
                 <Grid sx={{
                   display: "flex",
                   flexDirection: "row",
@@ -129,7 +182,7 @@ export default function SignIn({ doSmth }) {
                     OR
                   </Divider>
                 </Grid>
-                <Grid sx={{ display:'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', justifyContent: 'space-evenly' }}>
+                <Grid sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', justifyContent: 'space-evenly' }}>
                   <Button
                     type="button"
                     variant="contained"
@@ -155,13 +208,13 @@ export default function SignIn({ doSmth }) {
                     <TwitterIcon />
                   </Button>
                   <Button
-                  type="button"
-                  variant="contained"
-                  color='other'
-                  sx={{ mt: 2, mb: 2 }}
-                >
-                  <LinkedInIcon />
-                </Button>
+                    type="button"
+                    variant="contained"
+                    color='other'
+                    sx={{ mt: 2, mb: 2 }}
+                  >
+                    <LinkedInIcon />
+                  </Button>
                 </Grid>
               </Box>
             </Box>
--- login/src/pages/ResetPage.js
+++ login/src/pages/ResetPage.js
@@ -1,14 +1,30 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import image from '../assets/img/daiga-ellaby-JZ51o_-UOY8-unsplash.jpg'
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 export default function ResetPage({ doSmth }) {
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+
   return (
     <div>
       <Paper elevation={6} sx={{ my: 13, borderRadius: ".5em .5em .5em .5em", }}>
@@ -57,16 +73,35 @@ export default function ResetPage({ doSmth }) {
                 </Grid>
               </Grid>
               <Box component="form" noValidate sx={{ mt: 1 }}>
-                <TextField
-                  margin="normal"
+                <FormControl sx={{ mt: 1, }} margin="normal"
                   required
                   fullWidth
                   id="email"
                   label="Enter your registered email address"
                   name="email"
                   autoComplete="email"
-                  autoFocus
-                />
+                  variant="outlined"
+                  autoFocus>
+                  <InputLabel htmlFor="outlined-adornment-password">Enter your registered email address</InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showPassword ? 'text' : 'text'}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Enter your registered email address"
+                  />
+                </FormControl>
+                {/* <EmailIcon /> */}
                 <Button
                   type="submit"
                   fullWidth
